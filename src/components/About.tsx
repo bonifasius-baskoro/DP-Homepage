0b27a20c4f28535203c58d2dcae420e259b9ddd4
Jerry Baskoro <bjbbeat@gmail.com>
2024-10-18 17:38:35 +0700

    add about in homepage

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { FC, useState, useEffect, useRef } from "react";
 
 const About: FC = () => {
@@ -30,28 +30,29 @@ const About: FC = () => {
   }, []);
 
   return (
-    <div 
+    <div
       ref={ref}
       className={`h-screen px-2 bg-[#212121] pt-24 transition-all duration-1000 ease-in-out ${
-        isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
       }`}
     >
       <div>
-        <h1 className="text-5xl text-yellow-p px-2 md:px-4">
-          What We Do
-        </h1>
+        <h1 className="text-5xl text-yellow-p px-2 font-semibold md:px-4">What We Do</h1>
       </div>
       <div className="mt-6">
         <p className="text-yellow-p text-lg pt-8 px-2 md:px-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          The AI Agency, based in South Tangerang, is at the forefront of
+          Indonesia&apos;s artificial intelligence revolution. From this strategic
+          location in Banten province, the company leverages its expertise in AI
+          to drive innovation and efficiency across Indonesian businesses. Their
+          suite of services, including OCR and conversation agents, is designed
+          to reshape operations and boost productivity for companies throughout
+          the region. The AI Agency&apos;s team of experts embraces challenges,
+          constantly developing new AI solutions tailored to the unique
+          landscape of Indonesian commerce and society. By focusing on local
+          needs while applying global AI advancements, the company is working to
+          secure Indonesia&apos;s place in the digital future, all from its home base
+          in South Tangerang.
         </p>
       </div>
     </div>
